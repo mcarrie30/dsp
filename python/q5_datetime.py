07d59194955c7dbf643abbf9574b3859a26ee76a
Michael Carrie <mcarrie30@gmail.com>
2017-03-12 13:17:06 -0400

    added date

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -7,14 +7,12 @@ start = datetime.strptime('01-02-2013', '%m-%d-%Y')
 end = datetime.strptime('07-28-2015', '%m-%d-%Y')
 print(end-start)
 
-
 ####b)  
 start2 = datetime.strptime('12312013', '%m%d%Y')
 end2 = datetime.strptime('05282015', '%m%d%Y')
 print(end2-start2)
 
-
 ####c)
 start3 = datetime.strptime('15-Jan-1994', '%d-%b-%Y')
 end3 = datetime.strptime('14-Jul-2015', '%d-%b-%Y' )
-print(end3-start3) 
+print(end3-start3)
