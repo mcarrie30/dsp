c1b73c21eb7cddda8734be6c6c00f6a433bb37fc
Michael Carrie <mcarrie30@gmail.com>
2017-03-14 17:25:31 -0400

    advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,6 @@
-PLACE YOUR CODE HERE
+
+email_list = list(df['email'])
+with open("emails.csv", "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    for val in email_list:
+        writer.writerow([val])
