18772b8d31fd8c3d354b91db36ddbc0183ecee5f
Michael Carrie <mcarrie30@gmail.com>
2017-03-15 14:11:26 -0400

    Done Work

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -11,14 +11,16 @@
 df['lastname'] = df['name'].str.rsplit(' ', expand=True, n=1)[1]
 question6df = df.filter(['lastname','degree','title', 'email'], axis=1)
 dict1 = question6df.set_index('lastname').T.to_dict('list')
-print(dict1)
 
 ###Question 7 ###
 df['firstname'] = df['name'].str.split(' ', expand=True, n=1)[0]
 question7df = df.filter(['firstname','lastname','degree','title', 'email'], axis=1)
-dict2 = question7df.set_index(['firstname', 'lastname']).T.to_dict('list')
+question7dfsorted = question7df.sort('firstname')[0:3]
+dict2 = question7dfsorted.set_index(['firstname', 'lastname']).T.to_dict('list')
 dict2
 
 ###Question 8 ###
-dict3 = question7df.set_index(['lastname', 'firstname']).T.to_dict('list')
+question8df = question7df.loc[0:2]
+dict3 = question8df.set_index(['lastname', 'firstname']).T.to_dict('list')
+dict3estion8df.set_index(['lastname', 'firstname']).T.to_dict('list')
 dict3
