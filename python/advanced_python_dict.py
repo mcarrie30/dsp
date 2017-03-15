9875c56e7ef9c567c6313e6ed1a90a7f0365ba82
Michael Carrie <mcarrie30@gmail.com>
2017-03-15 13:42:20 -0400

    Code Shown

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,24 @@
-PLACE YOUR CODE HERE
+
+##### CODE FOR DATAFRAME IS IN advanced_python_regex.py #####
+
+
+#                 ^
+#                 |
+#                 |
+#                 |
+
+###Question 6 ###
+df['lastname'] = df['name'].str.rsplit(' ', expand=True, n=1)[1]
+question6df = df.filter(['lastname','degree','title', 'email'], axis=1)
+dict1 = question6df.set_index('lastname').T.to_dict('list')
+print(dict1)
+
+###Question 7 ###
+df['firstname'] = df['name'].str.split(' ', expand=True, n=1)[0]
+question7df = df.filter(['firstname','lastname','degree','title', 'email'], axis=1)
+dict2 = question7df.set_index(['firstname', 'lastname']).T.to_dict('list')
+dict2
+
+###Question 8 ***
+dict3 = question7df.set_index(['lastname', 'firstname']).T.to_dict('list')
+dict3
